5e017e33dbfa2e6d64e92c35bf05a1b55043de6c
idoit0 <895877057@qq.com>
2018-03-03 09:32:24 +0800

    提交

---
+++ java/基础/互联网并发编程/synchronized锁重入.java
@@ -0,0 +1,45 @@
+锁重入：
+    关键字 synchronized 拥有锁重入的功能，也就是在使用synchronized时，当一个线程得到一个对象的锁后，再次请求此对象时是可以再次得到该对象的锁。
+
+public class SyncDubbo2 {
+	static class Main {
+		public int i = 10;
+		public synchronized void operationSup(){
+			try {
+				i--;
+				System.out.println("Main print i = " + i);
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	static class Sub extends Main {
+		public synchronized void operationSub(){
+			try {
+				while(i > 0) {
+					i--;
+					System.out.println("Sub print i = " + i);
+					Thread.sleep(100);		
+					this.operationSup();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Sub sub = new Sub();
+				sub.operationSub();
+			}
+		});
+		
+		t1.start();
+	}
+}
