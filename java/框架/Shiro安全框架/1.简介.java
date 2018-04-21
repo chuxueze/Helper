af0497e5c8d3dfbf8b585e3f165499bb25bcb610
idoit0 <895877057@qq.com>
2018-04-21 15:17:25 +0800

    shrio-maven依赖

--- java/框架/Shiro安全框架/1.简介.java
+++ java/框架/Shiro安全框架/1.简介.java
@@ -1,3 +1,15 @@
 Shiro:
 1.Apache的强大灵活的开源安全框架
 2.功能（认证，授权，企业会话管理，安全加密，缓存）
+
+
+maven依赖引入
+
+	<dependencies>
+        <!-- https://mvnrepository.com/artifact/org.apache.shiro/shiro-core -->
+        <dependency>
+            <groupId>org.apache.shiro</groupId>
+            <artifactId>shiro-core</artifactId>
+            <version>1.4.0</version>
+        </dependency>
+    </dependencies>
