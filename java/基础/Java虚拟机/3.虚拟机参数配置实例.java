66dd81c480f699af136f4dd386418b30cda67150
idoit0 <895877057@qq.com>
2018-05-31 18:35:05 +0800

    Java虚拟机垃圾回收算法

---
+++ java/基础/Java虚拟机/3.虚拟机参数配置实例.java
@@ -0,0 +1,60 @@
+jdk版本 机器配置 建议jvm参数 备注
+jdk1.7 6V8G -server 
+-Xms4g -Xmx4g -Xmn2g -Xss768k -XX:PermSize=512m -XX:MaxPermSize=512m 
+-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSClassUnloadingEnabled 
+-XX:+DisableExplicitGC -XX:+UseCMSInitiatingOccupancyOnly 
+-XX:CMSInitiatingOccupancyFraction=68 -verbose:gc -XX:+PrintGCDetails 
+-Xloggc:{CATALINA_BASE}/logs/gc.log -XX:+PrintGCDateStamps 
+-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath={CATALINA_BASE}/logs 
+
+前台jdk1.7 8V8G -server 
+-Xms4g -Xmx4g -Xmn2g -Xss768k -XX:PermSize=512m -XX:MaxPermSize=512m 
+-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSClassUnloadingEnabled 
+-XX:+DisableExplicitGC -XX:+UseCMSInitiatingOccupancyOnly 
+-XX:CMSInitiatingOccupancyFraction=68 -verbose:gc -XX:+PrintGCDetails 
+-Xloggc:{CATALINA_BASE}/logs/gc.log -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError 
+-XX:HeapDumpPath={CATALINA_BASE}/logs 
+
+前台jdk1.7 4V8G -server 
+-Xms4g -Xmx4g -Xmn2g -Xss768k -XX:PermSize=512m -XX:MaxPermSize=512m 
+-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSClassUnloadingEnabled 
+-XX:+DisableExplicitGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=68 
+-verbose:gc -XX:+PrintGCDetails -Xloggc:{CATALINA_BASE}/logs/gc.log 
+-XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath={CATALINA_BASE}/logs 
+
+前台jdk1.7 6V8G -server 
+-Xms4g -Xmx4g -XX:MaxPermSize=512m \-verbose:gc -XX:+PrintGCDetails 
+-Xloggc￼{CATALINA_BASE}/logs/gc.log -XX:+PrintGCTimeStamps \ 后台
+
+
+配置说明：
+1. 堆设置
+o -Xms:初始堆大小
+o -Xmx:最大堆大小
+o -XX:NewSize=n:设置年轻代大小
+o -XX:NewRatio=n:设置年轻代和年老代的比值。
+如:为3，表示年轻代与年老代比值为1：3，年轻代占整个年轻代年老代和的1/4
+o -XX:SurvivorRatio=n:年轻代中Eden区与两个Survivor区的比值。
+注意Survivor区有两个。如：3，表示Eden：Survivor=3：2，一个Survivor区占整个年轻代的1/5
+o -XX:MaxPermSize=n:设置持久代大小
+
+2. 收集器设置
+o -XX:+UseSerialGC:设置串行收集器
+o -XX:+UseParallelGC:设置并行收集器
+o -XX:+UseParalledlOldGC:设置并行年老代收集器
+o -XX:+UseConcMarkSweepGC:设置并发收集器
+
+3. 垃圾回收统计信息 
+-XX:+PrintGC 
+-XX:+PrintGCDetails 
+-XX:+PrintGCTimeStamps 
+-Xloggc:filename
+
+4. 并行收集器设置 
+-XX:ParallelGCThreads=n:设置并行收集器收集时使用的CPU数。并行收集线程数。 
+-XX:MaxGCPauseMillis=n:设置并行收集最大暂停时间 
+-XX:GCTimeRatio=n:设置垃圾回收时间占程序运行时间的百分比。公式为1/(1+n)
+
+5. 并发收集器设置
+-XX:+CMSIncrementalMode:设置为增量模式。适用于单CPU情况。 
+-XX:ParallelGCThreads=n:设置并发收集器年轻代收集方式为并行收集时，使用的CPU数。并行收集线程数。
