c2b3bfc5c0b7c898b986cc60eae81374434c4925
idoit0 <895877057@qq.com>
2018-12-07 23:40:39 +0800

    Java类加载

--- A.Java/A.基础篇/A.Java虚拟机/G.深入理解Java虚拟机（JVM性能调优+内存模型+虚拟机原理）/00.类加载机制.java
+++ A.Java/A.基础篇/A.Java虚拟机/G.深入理解Java虚拟机（JVM性能调优+内存模型+虚拟机原理）/00.类加载机制.java
@@ -197,3 +197,133 @@ class [I
 
 Exception in thread "main" java.lang.ClassNotFoundException: [I
 ...
+
+
+
+--------------------------------------------------------
+9:钻石依赖:
+
+    项目管理上有1个著名的概念叫着「钻石依赖」，是指软件依赖导致同1个软件包的两个版本需要共存而不能冲突。
+
+    我们平时使用的 maven 是这样解决钻石依赖的，它会从多个冲突的版本中选择1个来使用，如果不同的版本之间兼容性很糟糕，那么程序将无法正常编译运行。Maven 
+这种形式叫「扁平化」依赖管理。
+
+    使用 ClassLoader 可以解决钻石依赖问题。不同版本的软件包使用不同的 ClassLoader 来加载，位于不同 ClassLoader 中名称1样的类实际上是不同的类。下面让我们使用 
+URLClassLoader 来尝试1个简单的例子，它默认的父加载器是 AppClassLoader
+
+$ cat ~/source/jcl/v1/Dep.java
+public class Dep {
+	public void print() {
+		System.out.println("v1");
+	}
+}
+
+$ cat ~/source/jcl/v2/Dep.java
+public class Dep {
+ public void print() {
+  System.out.println("v1");
+ }
+}
+
+$ cat ~/source/jcl/Test.java
+public class Test {
+	public static void main(String[] args) throws Exception {
+		String v1dir = "file:///Users/qianwp/source/jcl/v1/";
+		String v2dir = "file:///Users/qianwp/source/jcl/v2/";
+		URLClassLoader v1 = new URLClassLoader(new URL[]{new URL(v1dir)});
+		URLClassLoader v2 = new URLClassLoader(new URL[]{new URL(v2dir)});
+		
+  Class<?> depv1Class = v1.loadClass("Dep");
+		Object depv1 = depv1Class.getConstructor().newInstance();
+		depv1Class.getMethod("print").invoke(depv1);
+
+		Class<?> depv2Class = v2.loadClass("Dep");
+		Object depv2 = depv2Class.getConstructor().newInstance();
+		depv2Class.getMethod("print").invoke(depv2);
+	 
+  System.out.println(depv1Class.equals(depv2Class));
+ }
+}
+
+在运行之前，我们需要对依赖的类库进行编译
+$ cd ~/source/jcl/v1
+$ javac Dep.java
+$ cd ~/source/jcl/v2
+$ javac Dep.java
+$ cd ~/source/jcl
+$ javac Test.java
+$ java Test
+v1
+v2
+false
+
+在这个例子中如果两个 URLClassLoader 指向的路径是1样的，下面这个表达式还是 false，因为即使是同样的字节码用不同的 ClassLoader 加载出来的类都不能算同1个类
+
+depv1Class.equals(depv2Class)
+
+
+我们还可以让两个不同版本的 Dep 类实现同1个接口，这样可以避免使用反射的方式来调用 Dep 类里面的方法。
+
+Class<?> depv1Class = v1.loadClass("Dep");
+IPrint depv1 = (IPrint)depv1Class.getConstructor().newInstance();
+depv1.print()
+
+
+    ClassLoader 固然可以解决依赖冲突问题，不过它也限制了不同软件包的操作界面必须使用反射或接口的方式进行动态调用。Maven 没有这种限制，
+它依赖于虚拟机的默认懒惰加载策略，运行过程中如果没有显示使用定制的 ClassLoader，那么从头到尾都是在使用 AppClassLoader，而不同版本的同名类必须使用不同的 
+ClassLoader 加载，所以 Maven 不能完美解决钻石依赖。
+如果你想知道有没有开源的包管理工具可以解决钻石依赖的，我推荐你了解1下 sofa-ark，它是蚂蚁金服开源的轻量级类隔离框架。
+
+
+--------------------------------------------------------
+10:分工与合作:
+
+    这里我们重新理解1下 ClassLoader 的意义，它相当于类的命名空间，起到了类隔离的作用。位于同1个 ClassLoader 里面的类名是唯1的，不同的 ClassLoader 
+可以持有同名的类。ClassLoader 是类名称的容器，是类的沙箱。
+
+    不同的 ClassLoader 之间也会有合作，它们之间的合作是通过 parent 属性和双亲委派机制来完成的。parent 具有更高的加载优先级。除此之外，parent 
+还表达了1种共享关系，当多个子 ClassLoader 共享同1个 parent 时，那么这个 parent 里面包含的类可以认为是所有子 ClassLoader 共享的。这也是为什么 
+BootstrapClassLoader 被所有的类加载器视为祖先加载器，JVM 核心类库自然应该被共享。
+
+
+--------------------------------------------------------
+11:Thread.contextClassLoader:
+
+    如果你稍微阅读过 Thread 的源代码，你会在它的实例字段中发现有1个字段非常特别
+
+class Thread {
+  ...
+  private ClassLoader contextClassLoader;
+  
+  public ClassLoader getContextClassLoader() {
+    return contextClassLoader;
+  }
+  
+  public void setContextClassLoader(ClassLoader cl) {
+    this.contextClassLoader = cl;
+  }
+  ...
+}
+
+contextClassLoader 「线程上下文类加载器」，这究竟是什么东西？
+
+    首先 contextClassLoader 是那种需要显示使用的类加载器，如果你没有显示使用它，也就永远不会在任何地方用到它。你可以使用下面这种方式来显示使用它
+Thread.currentThread().getContextClassLoader().loadClass(name);
+
+    这意味着如果你使用 forName(string name) 方法加载目标类，它不会自动使用 contextClassLoader。那些因为代码上的依赖关系而懒惰加载的类也不会自动使用 
+contextClassLoader来加载。
+
+    其次线程的 contextClassLoader 默认是从父线程那里继承过来的，所谓父线程就是创建了当前线程的线程。程序启动时的 main 线程的 contextClassLoader 就是 
+AppClassLoader。这意味着如果没有人工去设置，那么所有的线程的 contextClassLoader 都是 AppClassLoader。
+
+    那这个 contextClassLoader 究竟是做什么用的？我们要使用前面提到了类加载器分工与合作的原理来解释它的用途。
+    它可以做到跨线程共享类，只要它们共享同1个 contextClassLoader。父子线程之间会自动传递 contextClassLoader，所以共享起来将是自动化的。
+如果不同的线程使用不同的 contextClassLoader，那么不同的线程使用的类就可以隔离开来。
+如果我们对业务进行划分，不同的业务使用不同的线程池，线程池内部共享同1个 contextClassLoader，线程池之间使用不同的 contextClassLoader，
+就可以很好的起到隔离保护的作用，避免类版本冲突。
+
+    如果我们不去定制 contextClassLoader，那么所有的线程将会默认使用 AppClassLoader，所有的类都将会是共享的。
+线程的 contextClassLoader 使用场合比较罕见，如果上面的逻辑晦涩难懂也不必过于计较。
+
+    JDK9 增加了模块功能之后对类加载器的结构设计做了1定程度的修改，不过类加载器的原理还是类似的，作为类的容器，它起到类隔离的作用，
+同时还需要依靠双亲委派机制来建立不同的类加载器之间的合作关系。
