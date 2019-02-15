47633d97e59cf95651d5abd12e3dd2ce29a7cfd6
zhanghaochun <895877057@qq.com>
2019-02-15 10:05:49 +0800

    简历

---
+++ A.Java/B.进阶篇/A.设计模式之禅/B.23种设计模式/A.单例模式.java
@@ -0,0 +1,53 @@
+1.简介:
+2.例子:
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------
+1.简介:
+1、单例类只能有1个实例。
+2、单例类必须自己创建自己的唯1实例。
+3、单例类必须给所有其他对象提供这1实例。
+
+
+
+
+---------------------------------------------------------------
+2.例子:
+public class SingleObject {
+ 
+   //创建 SingleObject 的一个对象
+   private static SingleObject instance = new SingleObject();
+ 
+   //让构造函数为 private，这样该类就不会被实例化
+   private SingleObject(){}
+ 
+   //获取唯一可用的对象
+   public static SingleObject getInstance(){
+      return instance;
+   }
+ 
+   public void showMessage(){
+      System.out.println("Hello World!");
+   }
+}
+
+public class SingletonPatternDemo {
+   public static void main(String[] args) {
+ 
+      //不合法的构造函数
+      //编译时错误：构造函数 SingleObject() 是不可见的
+      //SingleObject object = new SingleObject();
+ 
+      //获取唯一可用的对象
+      SingleObject object = SingleObject.getInstance();
+ 
+      //显示消息
+      object.showMessage();
+   }
+}
